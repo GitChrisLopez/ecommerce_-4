c4072909a6adeca62a3cc4def6a0e3a86543c1da
GitChrisLopez <sabine.fitch252379@potros.itson.edu.mx>
2025-10-15 21:38:53 -0700

    Metodos, Getters, Setters y constructor de Usuario. Rol agregado

--- src/main/java/Dominio/Usuario.java
+++ src/main/java/Dominio/Usuario.java
@@ -4,10 +4,118 @@
  */
 package Dominio;
 
+import Dominio.enums.Rol;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author chris
  */
 public class Usuario {
+    private int id;
+    private String nombre;
+    private String correo;
+    private String contrasenia;
+    private String telefono;
+    private Rol rol;
+    private List<Direccion> direcciones;
+    private Carrito carrito;
+    private boolean activo;
+    private List<Pedido> pedidos;
+
+    public Usuario(int id, String nombre, String correo, String contrasenia, String telefono, Rol rol, Carrito carrito, boolean activo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+        this.telefono = telefono;
+        this.rol = rol;
+        this.carrito = carrito;
+        this.activo = activo;
+        this.direcciones = new ArrayList<>();
+        this.pedidos = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public List<Direccion> getDirecciones() {
+        return direcciones;
+    }
+
+    public void setDirecciones(List<Direccion> direcciones) {
+        this.direcciones = direcciones;
+    }
+
+    public Carrito getCarrito() {
+        return carrito;
+    }
+
+    public void setCarrito(Carrito carrito) {
+        this.carrito = carrito;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+    
     
 }
