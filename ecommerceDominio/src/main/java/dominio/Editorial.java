912695baa783d49af830ddbb590fef4b180a1731
manuel-romo <manuel.romo253080@potros.itson.edu.mx>
2025-11-16 01:11:52 -0700

    Documentación de Editorial

--- ecommerceDominio/src/main/java/dominio/Editorial.java
+++ ecommerceDominio/src/main/java/dominio/Editorial.java
@@ -8,38 +8,80 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * Representa una Editorial que publica libros.
+ *
+ * @author Norma Alicia Beltrán Martín - 252102
+ * @author Oscar Adrián Castán López - 260318
+ * @author Chris Fitch Lopez - 252379
+ * @author Manuel Romo López - 253080
+ * 
+ * Fecha: 15/11/2025
+ * 
+ */
 @Entity
 @Table(name = "editoriales")
 public class Editorial {
     
+    /**
+     * Dato Long que representa el Id de la editorial.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column (name = "id_editorial")
     private long id;
     
+    /**
+     * Objeto String que representa el nombre de la Editorial.
+     */
     @Column (name = "nombre", length = 50, nullable = false)
     private String nombre;
 
+    /**
+     * Constructor por defecto.
+     */
     public Editorial(){
     }
     
+    /**
+     * Constructor que permite crear una Editorial, recibe los valores de todos
+     * sus atributos.
+     * @param id Dato Long que representa el Id de la Editorial.
+     * @param nombre Objeto String que representa el nombre de la Editorial.
+     */
     public Editorial(long id, String nombre) {
         this.id = id;
         this.nombre = nombre;
     }
 
+    /**
+     * Permite obetner el Id de la Editorial.
+     * @return Dato Long que representa el Id de la Editorial.
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Permite establecer el Id de la Editorial.
+     * @param id Dato Long que representa el Id de la Editorial.
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * Permite obtener el nombre de la Editorial.
+     * @return Objeto String que representa el nombre de la Editorial.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Permite establecer el nombre de la Editorial.
+     * @param nombre Objeto String que representa el nombre de la Editorial.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
